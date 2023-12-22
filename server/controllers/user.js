725f48d1ec2d093871d149429b3203551b72e5c6
greekeros <kanuanton@gmail.com>
2023-12-22 14:12:21 +0200

    changing encryption from hash to DES

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,6 +1,22 @@
-import bcrypt from 'bcrypt';
+import crypto from 'crypto';
 import jwt from 'jsonwebtoken';
 import User from '../models/user.js';
+
+// Helper function for DES encryption
+const encryptWithDES = (text, secretKey) => {
+    const cipher = crypto.createCipher('des', secretKey);
+    let encrypted = cipher.update(text, 'utf8', 'hex');
+    encrypted += cipher.final('hex');
+    return encrypted;
+};
+
+// Helper function for DES decryption
+const decryptWithDES = (encryptedText, secretKey) => {
+    const decipher = crypto.createDecipher('des', secretKey);
+    let decrypted = decipher.update(encryptedText, 'hex', 'utf8');
+    decrypted += decipher.final('utf8');
+    return decrypted;
+};
 
 export const signin = async (req, res) => {
     const { email, password } = req.body;
@@ -9,10 +25,13 @@ export const signin = async (req, res) => {
         if (!existingUser)
             return res.status(404).json({ message: "User doesn't exist." });
 
-        const isPasswordCorrect = await bcrypt.compare(password, existingUser.password);
+        // Decrypt the stored password using the same key used for encryption
+        const decryptedPassword = decryptWithDES(existingUser.password, 'yourSecretKey');
+        const isPasswordCorrect = password === decryptedPassword;
+
         if (!isPasswordCorrect)
             return res.status(400).json({ message: "Invalid credentials." });
-        
+
         const token = jwt.sign({ name: existingUser.name, email: existingUser.email, id: existingUser._id }, 'test', { expiresIn: "1h" });
         res.status(200).json({ result: existingUser, token: token });
     } catch (error) {
@@ -21,16 +40,20 @@ export const signin = async (req, res) => {
 }
 
 export const signup = async (req, res) => {
-    const { email, password, confirmPassword, firstName, lastName, role = "user" } = req.body;    
+    const { email, password, confirmPassword, firstName, lastName, role = "user" } = req.body;
 
     try {
         const existingUser = await User.findOne({ email });
         if (existingUser) {
             return res.status(400).json({ message: "User already exist." });
         }
+
         if (password !== confirmPassword)
             return res.status(400).json({ message: "Passwords don't match." });
-        const encryptedPassword = await bcrypt.hash(password, 12);
+
+        // Encrypt the password using DES encryption
+        const encryptedPassword = encryptWithDES(password, 'yourSecretKey');
+
         const result = await User.create({ email, password: encryptedPassword, name: `${firstName} ${lastName}`, role });
         const token = jwt.sign({ name: result.name, email: result.email, id: result._id }, 'test', { expiresIn: "1h" });
         res.status(200).json({ result: result, token: token });
@@ -43,7 +66,6 @@ export const microsoftSignin = async (req, res) => {
     const { email, name } = req.body;
     try {
         const existingUser = await User.findOne({ email });
-        
         res.status(200).json({ result: existingUser });
     } catch (error) {
         return res.status(500).json({ message: "Something went wrong." });
